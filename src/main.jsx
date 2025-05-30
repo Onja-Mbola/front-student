387ba074bc7f245e7401a8bc92eee799f1d223f2
onjambola <onja.mbola.andrianirimanjaka@gmail.com>
2025-05-31 00:01:47 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from './context/AuthContext';
 import { ThemeProviderWithMode } from './context/ThemeContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = '880880280299-peshf29euievprdhekta8fu2el6qli0d.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '207771219867-up2r2hjsv1k5jfu41la5f4bs9kg0r22p.apps.googleusercontent.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
